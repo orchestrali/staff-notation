2765411295d67f6e08ddafb476208edbd1b46d57
orchestrali <orchestrali@gmail.com>
2025-08-27 21:14:47 +0100

    Add files via upload

--- public/jquery.centre.js
+++ public/jquery.centre.js
@@ -3,7 +3,7 @@
 $.fn.centre = function() {
 	return this.each(function() {
 		var elem = $(this);
-		elem.css({position: 'fixed',
+		elem.css({position: 'absolute',
 			left: Math.max(0, (window.innerWidth - elem.outerWidth()) / 2),
 			top: Math.max(0, (window.innerHeight - elem.outerHeight()) / 2)});
 	});
